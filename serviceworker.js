da4133e739af99f1890e8ac1ec49d0d49f4a988f
phi-xi <163747259+phi-xi@users.noreply.github.com>
2024-03-19 18:13:12 +0100

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -10,7 +10,7 @@ let SERVICE_WORKER = {
     version: "",
     cacheName: "PhiXi",
     cacheFilesAppShell: [
-        "/error-404.html"   // ATTENTION error page must be first element in array!
+        "/error/404.html"   // ATTENTION error page must be first element in array!
     ],
     lastNotification: "",
     messageRefreshInterval: 15,    // in seconds
