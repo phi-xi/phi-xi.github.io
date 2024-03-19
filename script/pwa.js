5e0e0a36318ae29cca1f7c5828a608b762f568cd
phi-xi <163747259+phi-xi@users.noreply.github.com>
2024-03-19 17:04:52 +0100

    Update pwa.js

--- script/pwa.js
+++ script/pwa.js
@@ -1,42 +1,77 @@
-/*
+/*##########################################################################
 
         Progressive web application interface
 
         (c) PhiXi, mmXXiv
 
-*/
+    Provides an interface for a progressive web application.
+    Service worker serves resources always from network, if available,
+    otherwise from cache. Whenever a cached resource has been fetched
+    from network, it will be updated in the cache.
+    Any change in the version file triggers service worker update.
+    The cache is updated (=all cached resources are fetched again) when
+    the service worker receives the message 'cache-update'; note that
+    your application must implement this.
+    The cache is deleted when the service worker receives the message
+    'cache-clear'; note that your application must implement this.
+
+##########################################################################*/
 
 
-const PWA = ( ()=>{
+const PWA = ( function(){
+
+    const notificationIconURL = "/img/icons-pwa/icon-144.png",
+        servceWorkerFile = "/serviceworker.js",
+        versionFile = "/version.json";
+
+    let serviceWorkerVersion = "";
+
+    async function getLatestVersion(){
+        const res = await fetch( versionFile + "?" + Math.random().toString().slice(2) );
+        if ( res.ok ){
+            const txt = await res.text();
+            try {
+                const data = JSON.parse( txt );
+                return data.serviceWorker;
+            } catch(e){}
+        }
+        return false;
+    }
+
+    ( async ()=>{
+        serviceWorkerVersion = await getLatestVersion();
+    } )();
 
     return {
-
+        isStandalone: ()=>{
+            return window.matchMedia( "(display-mode: standalone)" ).matches;
+        },
+        log: (msg)=>{
+            console.log( "[PWA] " + msg );
+        },
+        requestPermissionNotify: ()=>{
+            Notification.requestPermission().then( (result)=>{
+                if ( result === "granted" ) PWA.log( "Notifications permission granted" );
+            } );
+        },
         serviceWorker: {
-            file: "/serviceworker.js",
-            getLatestVersion: async ()=>{
-                const res = await fetch( "version.json?" + Math.random().toString().slice(2) );
-                if ( res.ok ){
-                    const txt = await res.text();
-                    try {
-                        const data = JSON.parse( txt );
-                        //this.#serviceWorkerVersion = data.serviceWorker;
-                        return data.serviceWorker;
-                    } catch(e){}
-                }
+            file: servceWorkerFile,
+            getLatestVersion: ()=>{
+                return serviceWorkerVersion;
             },
             register: ()=>{
                 if ( "serviceWorker" in navigator ){
                     navigator.serviceWorker.register( PWA.serviceWorker.file, {
                         scope: "/"
                     } );
-                    navigator.serviceWorker.addEventListener( "message", async(event)=>{
-                        const msg = event.data;
-                        if ( msg.topic == "version" ){
-                            const ver = msg.text,
-                                serviceWorkerVersion = await PWA.serviceWorker.getLatestVersion();
-                            if ( serviceWorkerVersion != "" && ver != serviceWorkerVersion ){
+                    navigator.serviceWorker.addEventListener( "message", (event)=>{
+                        const msg = event.data,
+                            serviceWorkerVersion = PWA.serviceWorker.getLatestVersion();
+                        if ( msg.topic == "version" && serviceWorkerVersion != "" ){
+                            const ver = msg.text;
+                            if ( ver != serviceWorkerVersion ){
                                 PWA.log( "Update available" );
-                                navigator.serviceWorker.controller.postMessage( "clear_cache" );
+                                //navigator.serviceWorker.controller.postMessage( "cache-clear" );
                                 PWA.serviceWorker.unregister();
                                 setTimeout( ()=>{
                                     window.location.href = "/";
@@ -50,7 +85,17 @@ const PWA = ( ()=>{
                             PWA.log( msg.text );
                         }
                         if ( msg.topic == "message" ){
-                            new Notification( "Notification received", {body: msg.text, icon: ""} );
+                            const data = JSON.parse( msg.text );
+                            let cb = (e)=>{},
+                                config = {
+                                body: data.body,
+                                icon: notificationIconURL
+                            };
+                            if ( data.url != "" ) cb = (e)=>{
+                                e.preventDefault();
+                                window.open( data.url );
+                            };
+                            new Notification( data.title, config ).onclick = cb;
                         }
                     } );
                     navigator.serviceWorker.ready.then( (r)=>{
@@ -77,19 +122,9 @@ const PWA = ( ()=>{
                     PWA.log( "Error: serviceWorker interface not available" );
                 }
             }
-        },
-        requestPermissionNotify: ()=>{
-            Notification.requestPermission().then( (result)=>{
-                if ( result === "granted" ) PWA.log( "Notifications permission granted" );
-            } );
-        },
-        isStandalone: ()=>{
-            return window.matchMedia( "(display-mode: standalone)" ).matches;
-        },
-        log: (msg)=>{
-            console.log( "[PWA] " + msg );
         }
-
     };
 
 } )();
+
+PWA.serviceWorker.register();
